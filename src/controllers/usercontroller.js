713dd586147328f2de832b7650bc49163d65aafd
Gabriel Lira <devzando8182@gmail.com>
2021-05-29 23:37:37 -0300

    Mudandoa nomenclatura das váriaveis e retornando um novo dado

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -5,7 +5,6 @@ const { generationtoken } = require('../helpers/jwt')
 module.exports = {
     async create(req, res) {
         const { name, email, password } = req.body
-
         const hash = await encrypt(password)
         try {
             await connection('users').insert({ name, email, password: hash })
@@ -18,13 +17,13 @@ module.exports = {
 
     async login(req, res) {
         const {email, password} = req.body
-        const [hash] = await connection('users').where('email', email).select('password', 'id')
-        if(hash == undefined || null || '') res.status(401).send()
+        const [results] = await connection('users').where('email', email).select('password', 'id')
+        if(results == undefined || null || '') res.status(401).send()
 
-        const result = await decrypt(password, hash.password)
-        if(result) {
-            const token = await generationtoken(hash.id)
-            res.status(200).send({token})
+        const decodedhash = await decrypt(password, results.password)
+        if(decodedhash) {
+            const token = await generationtoken(results.id)
+            res.status(200).send({token, id: results.id})
         }else{
             res.status(401).send()
         }
